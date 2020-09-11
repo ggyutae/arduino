8ddae26be9c9ccedda23cb7d7035a56821681db2
mazellanko <33318075+mazellanko@users.noreply.github.com>
2020-09-11 15:39:22 +0900

    Add files via upload

---
+++ CDs_NodeMCU/CDs_NodeMCU.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int value = analogRead(A0);
+
+  Serial.print("CDs value = ");
+  Serial.println(value);
+
+  delay(1000);
+}
---
+++ DHT_NodeMCU/DHT_KY-016_.ino
@@ -0,0 +1,26 @@
+#include "DHT.h"
+DHT dht(D5, DHT11);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHT test!"));
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+}
